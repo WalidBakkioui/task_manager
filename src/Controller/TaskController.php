bc965ae39060219f74a199371546be0618f86c40
unknown <walid.bakkioui@hotmail.com>
2025-05-14 08:07:04 +0200

    task_manager

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -224,7 +224,6 @@ class TaskController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $em->persist($task);
             $em->flush();
-            $this->addFlash('success', 'Tâche ajoutée avec succès !');
             return $this->redirectToRoute('task_index');
         }
 
